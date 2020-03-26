5fda5fb0f02ff5123854db54c4cb49010b26b8eb
S A <25065328+shayan5@users.noreply.github.com>
2020-03-25 22:10:41 -0400

    renamed file from demo

---
+++ src/main/java/com/th2/treasurehunter2/ApplicationDriver.java
@@ -0,0 +1,13 @@
+package com.th2.treasurehunter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDriver {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationDriver.class, args);
+	}
+
+}
